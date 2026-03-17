f7ea28861a65d7078866d236ba09531d8d837762
Kiran V <kiranv20042@gmail.com>
2026-03-17 21:49:55 +0530

    Push all local files

---
+++ ASSIGNMENT-7/Student Manager/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import StudentManager from './StudentManager'
+
+function App() {
+  return (
+    <main className="app-container">
+      <StudentManager />
+    </main>
+  )
+}
+
+export default App
